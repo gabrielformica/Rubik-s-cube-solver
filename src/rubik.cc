6c45b78a24771ab0c9f53ccea405c3b281397233
gabrielformica <gabrielformica93@gmail.com>
2014-04-18 18:29:26 -0430

    turnFrontCW already implemented

--- src/rubik.cc
+++ src/rubik.cc
@@ -176,7 +176,23 @@ void Rubik::turnBottomCCW() {
   */
 
 void Rubik::turnFrontCW() {
+    //back up corners
+    unsigned char temp1 = this->left[2];
 
+    //turn corners
+    this->left[2] = this->swapXZ(this->right[0]);
+    this->right[0] = this->swapXZ(this->right[6]);
+    this->right[6] = this->swapXZ(this->left[4]);
+    this->left[4] = this->swapXZ(temp1);
+
+    //back up edges
+    temp1 = this->left[3];
+
+    //turn edges
+    this->left[3] = this->swapXZ(this->middle[0]);
+    this->middle[0] = this->swapXZ(this->right[7]);
+    this->right[7] = this->swapXZ(this->middle[1]);
+    this->middle[1] = this->swapXZ(temp1);
 }
 
 
@@ -196,7 +212,23 @@ void Rubik::turnFrontCCW() {
   */
 
 void Rubik::turnBackCW() {
-    
+    //back up corners
+    unsigned char temp1 = this->left[2];
+
+    //turn corners
+    this->left[2] = this->swapXZ(this->right[0]);
+    this->right[0] = this->swapXZ(this->right[6]);
+    this->right[6] = this->swapXZ(this->left[4]);
+    this->left[4] = this->swapXZ(temp1);
+
+    //back up edges
+    temp1 = this->left[3];
+
+    //turn edges
+    this->left[3] = this->swapXZ(this->middle[0]);
+    this->middle[0] = this->swapXZ(this->right[7]);
+    this->right[7] = this->swapXZ(this->middle[1]);
+    this->middle[1] = this->swapXZ(temp1);
 }
 
 
